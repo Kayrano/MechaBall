8e56af4bc44278bc67e943f20e935c3d3f087be9
Kayra Istar <Kayrano2221@hotmail.com>
2021-05-01 03:27:42 +0100

    *Spikes waits a second

--- Assets/Scripts/Spikes/SpikeScript.cs
+++ Assets/Scripts/Spikes/SpikeScript.cs
@@ -1,23 +1,39 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using Character;
-
+using System.Collections;
 
 [RequireComponent(typeof(PlayerScript))]
 public class SpikeScript : MonoBehaviour
 {
     
     public PlayerScript PlayerS;
+    private float targetTime;
+    private bool timerEnded = true;
+
+
+    private void Update()
+    {
+        if (timerEnded) { return; } 
+
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f)
+            timerEnded = true;
+    }
 
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
-        {
-            Debug.Log("Spike Collided with player!!!" + collision.collider.name);
-            PlayerS.playerH.TakeDamage();
-            
-        }
+        if(!collision.gameObject.CompareTag("Player")) { return; }
+
+        if (!timerEnded) { return; }
+
+        Debug.Log("Spike Collided with player!!!" + collision.collider.name);
+        PlayerS.playerH.TakeDamage();
+
+        targetTime = 1.2f;
+        timerEnded = false;
+        
+
         
     }
     
