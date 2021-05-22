60924dc9e4e730bdc546739ec1a99304f0adc591
Kayra Istar <Kayrano2221@hotmail.com>
2021-05-22 14:49:43 +0100

    *Teleport polishing, SFX

--- Assets/Prefabs/Teleport/PortalTeleport.cs
+++ Assets/Prefabs/Teleport/PortalTeleport.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Character;
 
 public class PortalTeleport : MonoBehaviour
 {
     [SerializeField] Transform portalExit;
     [SerializeField] GameObject player;
-    [SerializeField] int id;
+    bool hasEntered = false;
     
-
-    
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (!collision.gameObject.CompareTag("Player")) { return; }
 
+        if (hasEntered) { return; }
 
-        player.transform.position = portalExit.position;
+        hasEntered = true;
+
+        PlayerMovementScript playerM = player.GetComponent<PlayerMovementScript>();
+
+        playerM.enabled = false;
+
+        player.transform.position = new Vector3(portalExit.transform.position.x, portalExit.transform.position.y, -11);
+
+        Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
 
 
+        rb2d.velocity = new Vector2(0, 0);
+        rb2d.angularVelocity = 0;
 
 
     }
---
+++ Assets/Prefabs/Teleport/PortalTeleportExit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Character;
+
+public class PortalTeleportExit : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    [SerializeField] GameObject player;
+
+    bool hasExit = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasExit) { return; }
+
+        hasExit = true;
+        
+
+        audioSource.Play();
+
+        Invoke("BeVisible", 1f);
+
+        PlayerMovementScript playerM = player.GetComponent<PlayerMovementScript>();
+
+        playerM.enabled = true;
+
+
+    }
+
+    private void BeVisible()
+    {
+
+        player.transform.position = this.transform.position;
+
+
+    }
+
+
+}
